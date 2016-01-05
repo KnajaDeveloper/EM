a803cc1c9890c978cc3d8e78aac9f56c5ed9b6fd
pongpipat_s <its-pongpipat_s@internal.ssg>
2016-01-05 21:27:23 +0700

    gen class

---
+++ src/main/java/com/app2/app2t/domain/em/EMPosition.java
@@ -0,0 +1,19 @@
+package com.app2.app2t.domain.em;
+import com.app2.app2t.base.BaseEntity;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(inheritanceType = "TABLE_PER_CLASS")
+public class EMPosition extends BaseEntity {
+
+    /**
+     */
+    private String positionCode;
+
+    /**
+     */
+    private String positionName;
+}
---
+++ src/test/java/com/app2/app2t/domain/em/EMTeamDataOnDemand.java
@@ -0,0 +1,6 @@
+package com.app2.app2t.domain.em;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = EMTeam.class)
+public class EMTeamDataOnDemand {
+}
