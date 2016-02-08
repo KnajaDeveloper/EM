fece1904ae09ebeff0539058119db02108e3f52c
K_NAJA <pongpipat_s@softsquregroup.com>
2016-02-08 21:34:47 +0700

    confic project

--- src/main/java/com/app2/app2t/web/em/TestConficController.java
+++ src/main/java/com/app2/app2t/web/em/TestConficController.java
@@ -6,14 +6,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RequestMapping("/testfic")
 @Controller
 public class TestConficController {
 
+	protected Logger LOGGER = LoggerFactory.getLogger(TestConficController.class);
+
     @RequestMapping(value="/testfic",method = RequestMethod.GET, produces = "text/html")
     public String testfic(Model uiModel) {
-
+    	LOGGER.debug("in testfic");
         return "testfic/testfic";
     }
 
