0ce06eb240cb019c55aa877747e42331f4a8b438
Patiphan <Patiphan>
2016-03-29 17:28:07 +0700

    LOV

--- src/main/webapp/scripts/component/UtilLovEmp.js
+++ src/main/webapp/scripts/component/UtilLovEmp.js
@@ -10,7 +10,7 @@ function searchLovEmployee(text){
             Accept: "application/json"
         },
         type: "GET",
-        url: contextPath + '/ememployees/findEmployeeByText',
+        url: contextPath + '/central/findEmployeeByText',
         data : dataJsonData,
         complete: function(xhr){
             if(xhr.status === 201 || xhr.status === 200){
